0aa1ed35b0b41df383400839aaedcef56d6624d4
questcoderull <questcoderull@gmail.com>
2025-06-25 18:13:27 +0600

    navbar and footer added

--- src/Pages/ErrorPages/PageNotFound.jsx
+++ src/Pages/ErrorPages/PageNotFound.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { MdErrorOutline } from "react-icons/md";
+import { FaExclamationTriangle } from "react-icons/fa";
+import { Link } from "react-router";
 
 const PageNOtFound = () => {
   return (
-    // main code
-    <div className="min-h-screen flex items-center justify-center bg-base-200 px-6">
-      <div className="text-center space-y-6">
-        <div className="text-error">
-          <MdErrorOutline className="mx-auto text-[80px]" />
-        </div>
-
-        <h1 className="text-4xl font-bold text-error">404 - Page Not Found</h1>
-        <p className="text-base-content max-w-md mx-auto">
-          Sorry, the page you're looking for doesn't exist or may have been
-          moved.
-        </p>
-
-        <button className="btn btn-error text-white">Go to Homepage</button>
+    <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+      <div className="text-[100px] text-[#FB8500] mb-4">
+        <FaExclamationTriangle />
       </div>
+
+      <h1 className="text-6xl font-bold text-[#023047] mb-2">404</h1>
+      <h2 className="text-2xl font-semibold text-[#023047] mb-2">
+        Page Not Found
+      </h2>
+      <p className="text-[#023047] mb-6 max-w-md">
+        Sorry, the page you’re looking for doesn’t exist or may have been moved.
+      </p>
+
+      <Link
+        to="/"
+        className="btn border-none text-white bg-[#FB8500] hover:bg-[#FFB703] transition-colors duration-300"
+      >
+        ⬅ Go Back Home
+      </Link>
     </div>
   );
 };
--- src/Pages/Shared/Navbar.jsx
+++ src/Pages/Shared/Navbar.jsx
@@ -1,7 +1,229 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router";
+import {
+  FaHome,
+  FaPlus,
+  FaBlog,
+  FaStar,
+  FaHeart,
+  FaSignInAlt,
+  FaUserPlus,
+} from "react-icons/fa";
+import logo from "../../assets/readly.png";
 
 const Navbar = () => {
-  return <div></div>;
+  const [user, setUser] = useState(false);
+  const [loading, setLoading] = useState(false);
+  return (
+    <div className="navbar bg-base-100 shadow-sm sticky top-0 z-50 ">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-white rounded-box z-10 mt-3 w-52 p-2 shadow"
+          >
+            <li className="group border rounded-md mb-1 transition-colors duration-300 ease-in-out hover:bg-[#023047]">
+              <NavLink
+                to="/"
+                className="flex items-center space-x-2 px-3 py-2 text-[#003049] group-hover:text-white transition-colors duration-300 ease-in-out w-full"
+              >
+                <FaHome className="text-[#003049] group-hover:text-white transition-colors duration-300 ease-in-out" />
+                <span>Home</span>
+              </NavLink>
+            </li>
+
+            <li className="group border rounded-md mb-1 transition-colors duration-300 ease-in-out hover:bg-[#023047]">
+              <NavLink
+                to="/"
+                className="flex items-center space-x-2 px-3 py-2 text-[#0077a3] group-hover:text-white transition-colors duration-300 ease-in-out w-full"
+              >
+                <FaPlus className="text-[#0077a3] group-hover:text-white transition-colors duration-300 ease-in-out" />
+                <span>Add Blog</span>
+              </NavLink>
+            </li>
+
+            <li className="group border rounded-md mb-1 transition-colors duration-300 ease-in-out hover:bg-[#023047]">
+              <NavLink
+                to="/"
+                className="flex items-center space-x-2 px-3 py-2 text-[#669BBC] group-hover:text-white transition-colors duration-300 ease-in-out w-full"
+              >
+                <FaBlog className="text-[#669BBC] group-hover:text-white transition-colors duration-300 ease-in-out" />
+                <span>All Blogs</span>
+              </NavLink>
+            </li>
+
+            <li className="group border rounded-md mb-1 transition-colors duration-300 ease-in-out hover:bg-[#023047]">
+              <NavLink
+                to="/"
+                className="flex items-center space-x-2 px-3 py-2 text-[#F77F00] group-hover:text-white transition-colors duration-300 ease-in-out w-full"
+              >
+                <FaStar className="text-[#F77F00] group-hover:text-white transition-colors duration-300 ease-in-out" />
+                <span>Featured Blogs</span>
+              </NavLink>
+            </li>
+
+            <li className="group border rounded-md transition-colors duration-300 ease-in-out hover:bg-[#023047]">
+              <NavLink
+                to="/"
+                className="flex items-center space-x-2 px-3 py-2 text-[#D62828] group-hover:text-white transition-colors duration-300 ease-in-out w-full"
+              >
+                <FaHeart className="text-[#D62828] group-hover:text-white transition-colors duration-300 ease-in-out" />
+                <span>Wishlist</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div class="navbar-start">
+          <Link
+            to="/"
+            class="btn btn-ghost normal-case text-xl flex items-center gap-2"
+          >
+            <img src={logo} alt="Readly logo" class="h-8 w-auto" />
+            <span className="hidden md:block">Readly</span>
+          </Link>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1 p-2">
+          <li className="group border rounded-md mr-3 hover:bg-[#023047] transition-colors duration-300 ease-in-out flex items-center">
+            <NavLink
+              to="/"
+              className="flex items-center w-full text-[#003049] group-hover:text-white transition-colors duration-300 ease-in-out"
+            >
+              <FaHome className="mr-2 text-[#003049] group-hover:text-white transition-colors duration-300 ease-in-out" />
+              Home
+            </NavLink>
+          </li>
+          <li className="group border rounded-md mr-3 hover:bg-[#023047] transition-colors duration-300 ease-in-out flex items-center">
+            <NavLink
+              to="/"
+              className="flex items-center w-full text-[#0077a3] group-hover:text-white transition-colors duration-300 ease-in-out"
+            >
+              <FaPlus className="mr-2 text-[#0077a3] group-hover:text-white transition-colors duration-300 ease-in-out" />
+              Add Blog
+            </NavLink>
+          </li>
+          <li className="group border rounded-md mr-3 hover:bg-[#023047] transition-colors duration-300 ease-in-out flex items-center">
+            <NavLink
+              to="/"
+              className="flex items-center w-full text-[#669BBC] group-hover:text-white transition-colors duration-300 ease-in-out"
+            >
+              <FaBlog className="mr-2 text-[#669BBC] group-hover:text-white transition-colors duration-300 ease-in-out" />
+              All Blogs
+            </NavLink>
+          </li>
+          <li className="group border rounded-md mr-3 hover:bg-[#023047] transition-colors duration-300 ease-in-out flex items-center">
+            <NavLink
+              to="/"
+              className="flex items-center w-full text-[#F77F00] group-hover:text-white transition-colors duration-300 ease-in-out"
+            >
+              <FaStar className="mr-2 text-[#F77F00] group-hover:text-white transition-colors duration-300 ease-in-out" />
+              Featured Blogs
+            </NavLink>
+          </li>
+          <li className="group border rounded-md hover:bg-[#023047] transition-colors duration-300 ease-in-out flex items-center">
+            <NavLink
+              to="/"
+              className="flex items-center w-full text-[#D62828] group-hover:text-white transition-colors duration-300 ease-in-out"
+            >
+              <FaHeart className="mr-2 text-[#D62828] group-hover:text-white transition-colors duration-300 ease-in-out" />
+              Wishlist
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        {loading ? (
+          <span className="loading loading-spinner text-primary"></span>
+        ) : user ? (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                {/* <img
+                  alt="UserImage"
+                  src={
+                    user.photoURL ? user.photoURL : "https://ibb.co/VcGhBb0w"
+                  }
+                /> */}
+
+                {user && user.photoURL ? (
+                  <>
+                    <img
+                      data-tooltip-id="user-tooltip"
+                      data-tooltip-content={user.displayName}
+                      src={user.photoURL}
+                      alt="User profile"
+                      className="w-20 h-20 rounded-full object-cover cursor-pointer"
+                    />
+                    <Tooltip
+                      id="user-tooltip"
+                      place="bottom"
+                      variant="info"
+                      style={{ fontSize: "14px" }}
+                    />
+                  </>
+                ) : (
+                  <span>
+                    {user?.displayName?.charAt(0).toUpperCase() || "U"}
+                  </span>
+                )}
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-3 shadow border border-blue-400"
+            >
+              <li className="mb-4">
+                <p className="justify-between">{user.displayName}</p>
+              </li>
+              <li>
+                <button className="btn btn-neutral">Logout</button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className="flex items-center">
+            <NavLink
+              to="/login"
+              className="btn border hidden border-[#219EBC] text-[#023047] bg-white hover:bg-[#8ECAE6] hover:text-black rounded-md transition md:flex items-center gap-2"
+            >
+              <FaSignInAlt /> LogIn
+            </NavLink>
+
+            <NavLink
+              to="/signup"
+              className="btn border border-[#FB8500] bg-[#FB8500] text-white hover:bg-[#FFB703] ml-3 inline-flex items-center gap-2 md:px-4 px-2 py-1 text-sm"
+            >
+              <FaUserPlus />
+              <span className="hidden md:inline">SignUp</span>
+            </NavLink>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
