8fd6e4a1dbe2aef55150b8f71edcdb0f3b529e31
questcoderull <questcoderull@gmail.com>
2025-06-26 08:28:23 +0600

    added sign and signup page and animation in 404 page

--- src/Pages/ErrorPages/PageNotFound.jsx
+++ src/Pages/ErrorPages/PageNotFound.jsx
@@ -2,24 +2,25 @@ import React from "react";
 import { FaExclamationTriangle } from "react-icons/fa";
 import { Link } from "react-router";
 
+import animationNOtFound from "../../assets/404.json";
+import Lottie from "lottie-react";
+
 const PageNOtFound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
-      <div className="text-[100px] text-[#FB8500] mb-4">
-        <FaExclamationTriangle />
+      <div className="text-[100px] text-[#FB8500]">
+        <Lottie animationData={animationNOtFound} loop={true}></Lottie>
       </div>
 
-      <h1 className="text-6xl font-bold text-[#023047] mb-2">404</h1>
-      <h2 className="text-2xl font-semibold text-[#023047] mb-2">
-        Page Not Found
-      </h2>
-      <p className="text-[#023047] mb-6 max-w-md">
+      <h1 className="text-6xl font-bold text-[#023047] mb-2">Page Not Found</h1>
+
+      <p className="text-[#023047] mb-2 max-w-md">
         Sorry, the page you’re looking for doesn’t exist or may have been moved.
       </p>
 
       <Link
         to="/"
-        className="btn border-none text-white bg-[#FB8500] hover:bg-[#FFB703] transition-colors duration-300"
+        className="btn border-none text-white bg-[#FB8500] hover:bg-[#FFB703] transition-colors duration-300 mb-5"
       >
         ⬅ Go Back Home
       </Link>
